c154c867bbd86e4d1b442ec3e499de525edf181a
TIChing177 <164350842+TIChing177@users.noreply.github.com>
2024-04-20 06:36:44 +0000

    .

--- GOV_Python-SQL-main/lesson8/lesson8-3_sessionstate.py
+++ GOV_Python-SQL-main/lesson8/lesson8-3_sessionstate.py
@@ -5,18 +5,28 @@ st.write(st.session_state)
 
 #使用 input widget
 numbers:int = st.slider('value',min_value=1,max_value=10,key='myslider')
-st.write('加入sidebar後的session_state',st.session_state)
+#st.write('加入sidebar後的session_state',st.session_state)
 
 col1, buff, col2 = st.columns([1, 0.2,3])
+
+next = st.button('Next')
+if next:
+    if st.session_state.radio_option == "wolf":
+        st.session_state.radio_option = "bird"
+    elif st.session_state.radio_option == "bird":
+        st.session_state.radio_option = "ox"
+    else:
+        st.session_state.radio_option = "wolf"
+
 with col1:
     option_name = ['wolf','bird','ox']
     option = st.radio('choose one animal',option_name,key='radio_option')
-    st.write('加入radio後的session_state',st.session_state)
+    #st.write('加入radio後的session_state',st.session_state)
 
 with col2:
     if option == 'wolf':
         st.write(':wolf:')
-    if option == 'bird':
+    elif option == 'bird':
         st.write(':bird:')
-    if option == 'ox':
+    elif option == 'ox':
         st.write(':ox:')
