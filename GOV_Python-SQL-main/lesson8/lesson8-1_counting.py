893ccc536874dc508f19a99b764e388d19101698
TIChing177 <164350842+TIChing177@users.noreply.github.com>
2024-04-20 03:48:04 +0000

    counting

---
+++ GOV_Python-SQL-main/lesson8/lesson8-1_counting.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+# input 變數值一直被消滅，+1會失敗
+#st.title("計數器")
+#count = 0
+#increment = st.button("+1",key="plus1")
+#if increment:
+    #count +=1
+#st.write("計數器",count)
+
+#存 session_state 裡面可以保存+1值
+if 'count' not in st.session_state:
+    st.session_state['count'] = 0
+
+st.title("計數器")
+
+increment = st.button("+1",key="plus1")
+if increment:
+    st.session_state['count'] +=1
+
+st.write("計數器",st.session_state['count'])
