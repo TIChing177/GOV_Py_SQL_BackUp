a2f500f430ddd79906baaf5e3a02e592286554f2
TIChing177 <164350842+TIChing177@users.noreply.github.com>
2024-04-20 02:19:34 +0000

    .

--- GOV_Python-SQL-main/lesson7/lesson7_hw.py
+++ GOV_Python-SQL-main/lesson7/lesson7_hw.py
@@ -4,4 +4,28 @@ import psycopg2
 import os
 load_dotenv()
 
-#@st.cache_resource #增加效能
+@st.cache_resource #增加效能
+def get_names()->list:
+    with psycopg2.connect(os.environ['postgre_password']) as conn:
+        with conn.cursor()  as cursor:
+            sql = '''
+            SELECT DISTINCT stationname
+            FROM station
+            '''
+            cursor.execute(sql)
+            datas:list[tuple] = cursor.fetchmany(10)
+            names=[]
+            for item in datas:
+                names.append(item[0])
+            return names
+
+source_data = get_names()
+
+with st.sidebar:
+    option = st.selectbox(
+        'Station Name',
+        get_names(),
+        index=None,
+        placeholder='Choose a station'
+        )
+    st.write('當前站點',option)
