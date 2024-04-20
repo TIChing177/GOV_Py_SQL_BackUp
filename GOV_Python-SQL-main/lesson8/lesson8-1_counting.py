7b87ed0db2ed2332a7b99728e26c7899e655d91a
TIChing177 <164350842+TIChing177@users.noreply.github.com>
2024-04-20 03:52:08 +0000

    .

--- GOV_Python-SQL-main/lesson8/lesson8-1_counting.py
+++ GOV_Python-SQL-main/lesson8/lesson8-1_counting.py
@@ -10,12 +10,12 @@ import streamlit as st
 
 #存 session_state 裡面可以保存+1值
 if 'count' not in st.session_state:
-    st.session_state['count'] = 0
+    st.session_state.count = 0
 
 st.title("計數器")
 
 increment = st.button("+1",key="plus1")
 if increment:
-    st.session_state['count'] +=1
+    st.session_state.count +=1
 
-st.write("計數器",st.session_state['count'])
+st.write("計數器",st.session_state.count)
