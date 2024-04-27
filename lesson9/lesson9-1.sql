b12fa5a402977db5d5f1aab520650a4421b93065
TIChing177 <yoxi1717@gmail.com>
2024-04-27 10:58:48 +0800

    Update lesson9-1.sql

--- lesson9/lesson9-1.sql
+++ lesson9/lesson9-1.sql
@@ -8,3 +8,16 @@ create table if not exists 站點資訊(
 );
 
 select * from 站點資訊;
+
+create table if not exists youbike(
+	日期 timestamp,
+	編號 varchar(15),
+	總車輛 integer,
+	可借 integer,
+	可還 integer,
+	活動 boolean,
+	primary key(日期,編號),
+	foreign key(編號) references 站點資訊(站點編號)
+	on delete cascade
+	on update cascade
+);
